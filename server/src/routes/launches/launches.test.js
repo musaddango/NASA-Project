8b0ca5e20423498b980ed7bbbd5e899e21e17cf2
Musa Dango <musaddangoWgmail.com>
2023-11-15 20:43:49 +0100

    post test done

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -10,7 +10,31 @@ describe("Test GET /launches", ()=>{
 });
 
 describe("Test POST /launch", ()=>{
-    test("It should respond with 200 success", ()=>{});
-    test("It should catch Missing required launch property.",()=>{});
-    test("It should catch Invalid launch date input",()=>{});
-})
+
+    const completeSentData = {
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D",
+        target: "Keplar-186 f",
+        launchDate: "January 4, 2028",
+    }
+    const sentDataWithoutDate = {
+        mission: "USS Enterprise",
+        rocket: "NCC 1701-D",
+        target: "Keplar-186 f",
+    }
+    test("It should respond with 201 created", async ()=>{
+        const response = await request(app)
+            .post("/launches")
+            .send(completeSentData)
+            .expect("Content-Type",/json/)
+            .expect(201);
+
+        const responseDate = new Date(response.body.date).valueOf();
+        const requestDate = new Date(completeSentData.date).valueOf();
+        expect(responseDate).toBe(requestDate);
+        expect(response.body)
+        .toMatchObject(sentDataWithoutDate);
+    });
+    test("It should respond with 200 success",async ()=>{});
+    test("It should catch Invalid launch date input",async ()=>{});
+});
