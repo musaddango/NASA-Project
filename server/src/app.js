329d6da564ed38ff4fc68ee73faadaec08e7cbd5
Musa Dango <musaddangoWgmail.com>
2023-12-02 11:22:05 +0100

    Readjusting build file

--- server/src/app.js
+++ server/src/app.js
@@ -12,12 +12,12 @@ app.use(cors({
 }));
 app.use(morgan('combined'));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use('/v1',api);
 
 app.get('/*', (req, res)=>{
-    res.sendFile(path.join(path.join(__dirname, "..", "build", "index.html")))
+    res.sendFile(path.join(path.join(__dirname, "..", "public", "index.html")))
 })
 
 
